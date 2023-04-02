875fdae69facae483b701c69893e0d1be47f4c21
hyuti <lehuy.hl27@gmail.com>
2023-04-02 15:31:59 +0700

    feat: chat

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,7 +1,20 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:speech_to_text/speech_to_text.dart' as sttxt;
 import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+import 'package:file_picker/file_picker.dart';
+import 'package:uuid/uuid.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mime/mime.dart';
+import 'package:open_filex/open_filex.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,30 +24,230 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
   var _sttxt = sttxt.SpeechToText();
-  String text = "Press the button to speak";
+  String text = "";
   bool _speechEnabled = false;
 
   @override
   void initState() {
     super.initState();
     _initSpeech();
+    _loadMessages();
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleAttachmentPressed() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: 144,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleImageSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Photo'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleFileSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('File'),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Cancel'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
+    );
+
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: const Uuid().v4(),
+        mimeType: lookupMimeType(result.files.single.path!),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleImageSelection() async {
+    final result = await ImagePicker().pickImage(
+      imageQuality: 70,
+      maxWidth: 1440,
+      source: ImageSource.gallery,
+    );
+
+    if (result != null) {
+      final bytes = await result.readAsBytes();
+      final image = await decodeImageFromList(bytes);
+
+      final message = types.ImageMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        height: image.height.toDouble(),
+        id: const Uuid().v4(),
+        name: result.name,
+        size: bytes.length,
+        uri: result.path,
+        width: image.width.toDouble(),
+      );
+
+      _addMessage(message);
+    }
+  }
+
+  void _handleMessageTap(BuildContext _, types.Message message) async {
+    if (message is types.FileMessage) {
+      var localPath = message.uri;
+
+      if (message.uri.startsWith('http')) {
+        try {
+          final index =
+              _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+              (_messages[index] as types.FileMessage).copyWith(
+            isLoading: true,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+
+          final client = http.Client();
+          final request = await client.get(Uri.parse(message.uri));
+          final bytes = request.bodyBytes;
+          final documentsDir = (await getApplicationDocumentsDirectory()).path;
+          localPath = '$documentsDir/${message.name}';
+
+          if (!File(localPath).existsSync()) {
+            final file = File(localPath);
+            await file.writeAsBytes(bytes);
+          }
+        } finally {
+          final index =
+              _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+              (_messages[index] as types.FileMessage).copyWith(
+            isLoading: null,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+        }
+      }
+
+      await OpenFilex.open(localPath);
+    }
+  }
+
+  void _handlePreviewDataFetched(
+    types.TextMessage message,
+    types.PreviewData previewData,
+  ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
+  }
+
+  types.TextMessage _buildTextMsgWithStr(String message) {
+    return types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message,
+    );
+  }
+
+  types.TextMessage _buildTextMsg(types.PartialText message) {
+    return _buildTextMsgWithStr(message.text);
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    if (_sttxt.isNotListening) {
+      final textMessage = _buildTextMsg(message);
+      _addMessage(textMessage);
+    }
+  }
+
+  void _loadMessages() async {
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    setState(() {
+      _messages = messages;
+    });
   }
 
   void _onSpeechResult(SpeechRecognitionResult result) {
-    print(result);
     setState(() {
-      text = result.recognizedWords;
+      text += result.recognizedWords;
     });
   }
 
   void _initSpeech() async {
-    _speechEnabled = await _sttxt.initialize();
+    _speechEnabled = await _sttxt.initialize(
+      onStatus: (status) {
+        print(status);
+        if (status == "notListening") {
+          _stopListening();
+        }
+      },
+      onError: (errorNotification) {
+        print(errorNotification);
+        _stopListening();
+      },
+    );
     setState(() {});
   }
 
   void _startListening() async {
-    await _sttxt.listen(onResult: _onSpeechResult);
+    await _sttxt.listen(
+      onResult: _onSpeechResult,
+    );
     setState(() {});
   }
 
@@ -44,7 +257,12 @@ class _HomePageState extends State<HomePage> {
   /// listen method.
   void _stopListening() async {
     await _sttxt.stop();
-    setState(() {});
+    final textMessage = _buildTextMsgWithStr(text);
+    _addMessage(textMessage);
+
+    setState(() {
+      text = "";
+    });
   }
 
   @override
@@ -54,32 +272,55 @@ class _HomePageState extends State<HomePage> {
         title: Text("Speech to text"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Text(
-              _sttxt.isListening
-                  ? '$text'
-                  : _speechEnabled
-                      ? "Tap the microphone to start listening..."
-                      : "Speech not available",
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-            ),
-          ),
+      // body: Column(
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   mainAxisAlignment: MainAxisAlignment.end,
+      //   children: [
+      //     SingleChildScrollView(
+      //       child: Container(
+      //         child: Padding(
+      //           padding: const EdgeInsets.all(12.0),
+      //           child: Text(
+      //             _sttxt.isListening
+      //                 ? '$text'
+      //                 : _speechEnabled
+      //                     ? "Tap the microphone to start listening..."
+      //                     : "Speech not available",
+      //             style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //     const Padding(
+      //       padding: EdgeInsets.symmetric(horizontal: 8, vertical: 74),
+      //       child: TextField(
+      //         decoration: InputDecoration(
+      //             border: OutlineInputBorder(),
+      //             hintText: "Enter a search term"),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 74),
+        child: Chat(
+          theme: const DefaultChatTheme(primaryColor: Colors.blue),
+          messages: _messages,
+          onAttachmentPressed: _handleAttachmentPressed,
+          onMessageTap: _handleMessageTap,
+          onPreviewDataFetched: _handlePreviewDataFetched,
+          onSendPressed: _handleSendPressed,
+          showUserAvatars: true,
+          showUserNames: true,
+          user: _user,
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: AvatarGlow(
-          animate: _sttxt.isListening,
-          repeat: true,
-          endRadius: 80,
-          duration: Duration(milliseconds: 1000),
-          glowColor: Colors.blue,
-          child: FloatingActionButton(
-            onPressed: _sttxt.isListening ? _stopListening : _startListening,
-            child: Icon(_sttxt.isListening ? Icons.mic_off : Icons.mic),
-          )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sttxt.isListening ? _stopListening : _startListening,
+        child: Icon(_sttxt.isListening ? Icons.mic_off : Icons.mic),
+        elevation: 0.0,
+      ),
     );
   }
 }
